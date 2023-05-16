83f6233cafac4ae4572430fd8adf3f06f6adc7c5
vaibhawvipul <vaibhaw.vipul@gmail.com>
2023-05-16 20:04:42 +0530

    1. bug fixes 2. cargo run and cargo build works now 3. changed the cargo name to snakecase

--- src/cache.rs
+++ src/cache.rs
@@ -1,52 +1,68 @@
-use std::{collections::HashMap};
-use std::rc::Rc;
-use crate::evictionpolicy::{EvictionPolicy, LruPolicy};
+use std::collections::HashMap;
+use std::hash::Hash;
+use crate::evictionpolicy::EvictionPolicy;
 
-const CACHE_CAPACITY: usize = 1024;
-
-pub struct Cache<K, V>
+pub struct Cache<K, V, P>
 where
     P: EvictionPolicy<K, V>,
-    K: Eq + std::hash::Hash + Clone,
-    V: Default,
+    K: Eq + Hash + Clone,
+    V: Default + Clone,
 {
     capacity: usize,
-    data: HashMap<K, (Rc<V>, usize)>,
-    policy: Box<dyn EvictionPolicy<K, Rc<V>>>,
+    data: HashMap<K, (V, usize)>,
+    policy: P,
 }
 
-impl<K, V> Cache<K, V>
+impl<K, V, P> Cache<K, V, P>
 where
     P: EvictionPolicy<K, V>,
-    K: Eq + std::hash::Hash + Clone,
-    V: Default,
+    K: Eq + Hash + Clone,
+    V: Default + Clone,
 {
-    pub fn new() -> Self {
+    pub fn new(policy: P, capacity: usize) -> Self {
         Cache {
             data: HashMap::new(),
-            policy: Box::new(LruPolicy::new()),
-            capacity: CACHE_CAPACITY,
+            policy,
+            capacity,
         }
     }
 
-    pub fn get(&mut self, key: &K) -> Option<Rc<V>> {
+    pub fn with_data(policy: P, capacity: usize, data: HashMap<K, (V, usize)>) -> Self {
+        Cache {
+            data,
+            policy,
+            capacity,
+        }
+    }
+
+    pub fn get(&mut self, key: &K) -> Option<V> {
         if let Some((value, _)) = self.data.get(key).cloned() {
             self.policy.on_access(&mut self.data, key);
             Some(value.clone())
         } else {
             None
         }
-    }
+    }      
 
     pub fn insert(&mut self, key: K, value: V) {
         self.policy.on_insert(&mut self.data, &key);
-        let rc_value = Rc::new(value);
-        self.data.insert(key, (rc_value, self.data.len()));
+        self.data.insert(key, (value, self.data.len()));
+        if self.data.len() > self.capacity {
+            self.evict();
+        }
     }
 
-    pub fn remove(&mut self, key: &K) -> Option<Rc<V>> {
-        let value = self.data.remove(key)?;
-        Some(value.0)
+    pub fn remove(&mut self, key: &K) -> Option<V> {
+        if let Some((value, _)) = self.data.remove(key) {
+            Some(value)
+        } else {
+            None
+        }
+    }
+
+    fn evict(&mut self) {
+        let key = self.policy.on_evict(&mut self.data);
+        self.data.remove(&key);
     }
 
     pub fn len(&self) -> usize {
